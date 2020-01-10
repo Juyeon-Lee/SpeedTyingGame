a50c5114cab1a7a9592a0d866f462eab400603b4
ggj0418 <ggj0418@naver.com>
2020-01-10 14:35:05 +0900

    점수 조회 다이얼로그 추가

---
+++ SpeedTypingCnt/SpeedTypingCnt/ScoreRef.cpp
@@ -0,0 +1,34 @@
+﻿// ScoreRef.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "SpeedTypingCnt.h"
+#include "ScoreRef.h"
+#include "afxdialogex.h"
+
+
+// CScoreRef 대화 상자
+
+IMPLEMENT_DYNAMIC(CScoreRef, CDialogEx)
+
+CScoreRef::CScoreRef(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CScoreRef::~CScoreRef()
+{
+}
+
+void CScoreRef::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CScoreRef, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CScoreRef 메시지 처리기
