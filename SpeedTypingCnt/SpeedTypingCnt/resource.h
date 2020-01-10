273e05efbc683b69266a185bae34c28ed4dd39bb
jjw <jiwothur@naver.com>
2020-01-10 15:43:14 +0900

    hh

--- SpeedTypingCnt/SpeedTypingCnt/resource.h
+++ SpeedTypingCnt/SpeedTypingCnt/resource.h
@@ -9,6 +9,8 @@
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG_LOGIN                130
 #define IDD_DIALOG_SCORE                134
+#define IDD_DIALOG1                     136
+#define IDD_DIALOG_SOLO                 136
 #define IDC_BUTTON_SOLO                 1000
 #define IDC_EDIT_ID                     1001
 #define IDC_BUTTON_INIT                 1001
@@ -25,7 +27,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_RESOURCE_VALUE        138
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
