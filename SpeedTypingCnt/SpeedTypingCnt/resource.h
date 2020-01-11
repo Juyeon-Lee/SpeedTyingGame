12c0b82d7ae9a49d90955481166b901ee452cdd1
ggj0418 <ggj0418@naver.com>
2020-01-11 16:42:28 +0900

    대결 게임에서 게임 시스템 구축

--- SpeedTypingCnt/SpeedTypingCnt/resource.h
+++ SpeedTypingCnt/SpeedTypingCnt/resource.h
@@ -43,6 +43,7 @@
 #define IDC_STATIC14                    1028
 #define IDC_STATIC15                    1029
 #define IDC_EDIT_IP                     1030
+#define IDC_STATIC_SCORETEXT            1030
 #define IDC_STATIC_9                    1031
 #define IDC_STATIC_MAINID               1032
 
@@ -52,7 +53,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        140
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_CONTROL_VALUE         1031
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
